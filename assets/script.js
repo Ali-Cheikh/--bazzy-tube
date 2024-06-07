cf6e93ae3f5a76ebd325957d53a98931e3bb4dec
Ali <alouloucheikh05@gmail.com>
2024-06-08 00:12:49 +0100

    "Updated settings.json, script.js, and contact.html files with new code and formatting changes."

--- assets/script.js
+++ assets/script.js
@@ -1,61 +1,66 @@
+// const scriptURL = 'https://script.google.com/macros/s/AKfycby3TtDwoJ2lZULzYAHThDkIXufnpR-_U1ivqUVlUpdnevZ_XjVzTJxqVGPZTcsMsf_P/exec';
 
+// document.addEventListener('DOMContentLoaded', function () {
+//     const form = document.forms['contact-form'];
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycby1zuasNNH-2V19qLpazYxre154j35hotmZsP9DyOhSQbXMDgRnbQDMeOJGo8sjcNDg/exec';
+//     form.addEventListener('submit', e => {
+//         e.preventDefault();
 
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.forms['contact-form'];
+//         Swal.fire({
+//             icon: 'info',
+//             title: 'Please wait...',
+//             text: 'Sending your message',
+//             willOpen: () => {
+//                 Swal.showLoading();
+//             },
+//             showConfirmButton: false,
+//             allowOutsideClick: false,
+//             background: "hsla(231, 10%, 14%, 1)"
+//         });
 
-    form.addEventListener('submit', e => {
-        e.preventDefault();
+//         const formData = new FormData(form);
 
-        // Show SweetAlert immediately when the submit button is clicked
-        Swal.fire({
-            icon: 'info',
-            title: 'Please wait...',
-            text: 'Sending your message',
-            willOpen: () => {
-                Swal.showLoading();
-              },
-            showConfirmButton: false,
-            allowOutsideClick: false,
-            background:"hsla(231, 10%, 14%, 1)"
-        });
+//         fetch(scriptURL, {
+//             method: 'POST',
+//         })
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log("Response from Google Sheets:", data);
 
-        // Make the fetch request to the server
-        fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-        .then(response => {
-            // Handle response here
-            Swal.fire({
-                imgUrl:"assets/images/logo.png",
-                imageAlt: "Custom Success Icon",
-                title: 'Success!',
-                text: 'Your message is sent. Thank you for reaching out.',
-                width: 600,
-                padding: "3em",
-                timer: 5000, // Set timer for 5 seconds
-                timerProgressBar: true,
-                showConfirmButton: false,
-                allowOutsideClick: false,
-                color: "#000",
-                background: "#60c0e0",
-                backdrop: `
-                    hsla(231, 10%, 14%, 1)
-                    url("assets/images/logo.png")
-                    center top
-                    no-repeat
-                `,
-            }).then(() => {
-                form.submit();
-                window.location.reload(); }); // Reload the page after successful submission
-        })
-        .catch(error => {
-            console.error('Error!', error.message);
-            // Show an error alert if there's an issue with sending the message
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: 'Something went wrong! Please try again later.',
-            });
-        });
-    });
-});
+//             if (data.result === 'success') {
+//                 Swal.fire({
+//                     imageUrl: "assets/images/logo.png",
+//                     imageAlt: "Custom Success Icon",
+//                     title: 'Success!',
+//                     text: 'Your message is sent. Thank you for reaching out.',
+//                     width: 600,
+//                     padding: "3em",
+//                     timer: 5000,
+//                     timerProgressBar: true,
+//                     showConfirmButton: false,
+//                     allowOutsideClick: false,
+//                     color: "#000",
+//                     background: "#60c0e0",
+//                     backdrop: "rgba(0,0,0,0.4)",
+//                 }).then(() => {
+//                     window.location.reload();
+//                 });
+//             } else {
+//                 console.error('Error:', data.error);
+//                 Swal.fire({
+//                     icon: 'error',
+//                     title: 'Oops...',
+//                     text: 'Something went wrong! Please try again later.',
+//                 });
+//             }
+//         })
+//         .catch(error => {
+//             console.error('Error!', error.message);
+//             Swal.fire({
+//                 icon: 'error',
+//                 title: 'Oops...',
+//                 text: 'Something went wrong! Please try again later.',
+//             });
+//         });
+//     });
+// });
