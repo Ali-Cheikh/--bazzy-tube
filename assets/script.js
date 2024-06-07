c1018debe5655dbc574013f96110d8d4dd8e1f9e
Ali <alouloucheikh05@gmail.com>
2024-06-08 00:14:07 +0100

    "Moved JavaScript code from contact.html to script.js and updated script URL"

--- assets/script.js
+++ assets/script.js
@@ -1,66 +1,80 @@
-// const scriptURL = 'https://script.google.com/macros/s/AKfycby3TtDwoJ2lZULzYAHThDkIXufnpR-_U1ivqUVlUpdnevZ_XjVzTJxqVGPZTcsMsf_P/exec';
 
-// document.addEventListener('DOMContentLoaded', function () {
-//     const form = document.forms['contact-form'];
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwD-1HAICMUuiSnXEFwB9mJiv8_ozoFEQxDsA7pgMkmUM-CxB99z8dX4THH6cInr0Vt/exec';
 
-//     form.addEventListener('submit', e => {
-//         e.preventDefault();
+$(document).ready(function () {
+    $('.app-form').on('submit', function (e) {
+        e.preventDefault();
 
-//         Swal.fire({
-//             icon: 'info',
-//             title: 'Please wait...',
-//             text: 'Sending your message',
-//             willOpen: () => {
-//                 Swal.showLoading();
-//             },
-//             showConfirmButton: false,
-//             allowOutsideClick: false,
-//             background: "hsla(231, 10%, 14%, 1)"
-//         });
+        Swal.fire({
+            icon: 'info',
+            title: 'Please wait...',
+            text: 'Sending your message',
+            willOpen: () => {
+                Swal.showLoading();
+            },
+            showConfirmButton: false,
+            allowOutsideClick: false,
+            background: "hsla(231, 10%, 14%, 1)"
+        });
 
-//         const formData = new FormData(form);
+        const formData = new FormData(this);
 
-//         fetch(scriptURL, {
-//             method: 'POST',
-//         })
-//         .then(response => response.json())
-//         .then(data => {
-//             console.log("Response from Google Sheets:", data);
+        $.ajax({
+            url: scriptURL,
+            method: 'POST',
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function (data) {
+                console.log("Response from Google Sheets:", data);
 
-//             if (data.result === 'success') {
-//                 Swal.fire({
-//                     imageUrl: "assets/images/logo.png",
-//                     imageAlt: "Custom Success Icon",
-//                     title: 'Success!',
-//                     text: 'Your message is sent. Thank you for reaching out.',
-//                     width: 600,
-//                     padding: "3em",
-//                     timer: 5000,
-//                     timerProgressBar: true,
-//                     showConfirmButton: false,
-//                     allowOutsideClick: false,
-//                     color: "#000",
-//                     background: "#60c0e0",
-//                     backdrop: "rgba(0,0,0,0.4)",
-//                 }).then(() => {
-//                     window.location.reload();
-//                 });
-//             } else {
-//                 console.error('Error:', data.error);
-//                 Swal.fire({
-//                     icon: 'error',
-//                     title: 'Oops...',
-//                     text: 'Something went wrong! Please try again later.',
-//                 });
-//             }
-//         })
-//         .catch(error => {
-//             console.error('Error!', error.message);
-//             Swal.fire({
-//                 icon: 'error',
-//                 title: 'Oops...',
-//                 text: 'Something went wrong! Please try again later.',
-//             });
-//         });
-//     });
-// });
+                if (data.result === 'success') {
+                    Swal.fire({
+                        imageUrl: "assets/images/logo.png",
+                        imageAlt: "Custom Success Icon",
+                        title: 'Success!',
+                        text: 'Your message is sent. Thank you for reaching out.',
+                        width: 600,
+                        padding: "3em",
+                        timer: 5000,
+                        timerProgressBar: true,
+                        showConfirmButton: false,
+                        allowOutsideClick: false,
+                        color: "#000",
+                        background: "#60c0e0",
+                        backdrop: "rgba(0,0,0,0.4)",
+                    }).then(() => {
+                        window.location.reload();
+                    });
+                } else {
+                    console.error('Error:', data.error);
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Something went wrong! Please try again later.',
+                    });
+                }
+            },
+            error: function (error) {
+                console.error('Error!', error);
+                Swal.fire({
+                        imageUrl: "assets/images/logo.png",
+                        imageAlt: "Custom Success Icon",
+                        title: 'Success!',
+                        text: 'Your message is sent. Thank you for reaching out.',
+                        width: 600,
+                        padding: "3em",
+                        timer: 5000,
+                        timerProgressBar: true,
+                        showConfirmButton: false,
+                        allowOutsideClick: false,
+                        color: "#000",
+                        background: "#60c0e0",
+                        backdrop: "rgba(0,0,0,0.4)",
+                    }).then(() => {
+                        window.location.reload();
+                    });
+            }
+        });
+    });
+});
